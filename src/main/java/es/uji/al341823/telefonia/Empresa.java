9837246bf276966d18b7484aa9531a49fba0b43e
Juanjo González Abril <juanjo@gonzabril.es>
2017-02-21 11:52:14 +0100

    Update

--- src/main/java/es/uji/al341823/telefonia/Empresa.java
+++ src/main/java/es/uji/al341823/telefonia/Empresa.java
@@ -1,7 +1,16 @@
 package es.uji.al341823.telefonia;
 
+import java.time.LocalDateTime;
+
 /**
  * Created by Juanjo on 21/2/2017.
  */
 public class Empresa {
+
+    private String nombre;
+    private String nif;
+    private Direccion direccion;
+    private String email;
+    private LocalDateTime fechaAlta;
+    private Tarifa tarifa;
 }
--- src/main/java/es/uji/al341823/telefonia/Telefonia.java
+++ src/main/java/es/uji/al341823/telefonia/Telefonia.java
@@ -1,10 +1,16 @@
 package es.uji.al341823.telefonia;
 
+import es.uji.www.GeneradorDatosINE;
+
 /**
  * Created by Juanjo on 21/2/2017.
  */
 public class Telefonia {
     public static void main(String[] args) {
+        GeneradorDatosINE generador = new GeneradorDatosINE();
 
+        for (int i = 0; i < 1000; i++) {
+            System.out.println(generador.getNIF());
+        }
     }
 }
