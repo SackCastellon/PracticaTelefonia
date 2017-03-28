7e17e5e44a9aafc062aa79f3b3295a8bdbefd222
David Agost Monte <al341819@aula1197act.sg.uji.es>
2017-03-28 12:11:37 +0200

    update

---
+++ src/main/java/es/uji/al341823/telefonia/facturacion/tarifas/TarifaTardes.java
@@ -0,0 +1,10 @@
+package es.uji.al341823.telefonia.facturacion.tarifas;
+
+/**
+ * Created by al341819 on 28/03/17.
+ */
+public class TarifaTardes extends TarifaExtra{
+
+    public TarifaTardes(Tarifa tarifaBase, float precio){ super(tarifaBase, precio); }
+
+}
