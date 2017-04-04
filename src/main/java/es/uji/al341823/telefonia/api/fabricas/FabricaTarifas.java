ee164344a6b8427a141428d06b1f28078dfb33c1
DavidAgost <al341819@uji.es>
2017-04-04 11:35:15 +0200

    ACTUALIZADO

---
+++ src/main/java/es/uji/al341823/telefonia/api/fabricas/FabricaTarifas.java
@@ -0,0 +1,24 @@
+package es.uji.al341823.telefonia.api.fabricas;
+
+import es.uji.al341823.telefonia.facturacion.tarifas.Tarifa;
+import es.uji.al341823.telefonia.facturacion.tarifas.TarifaBasica;
+import es.uji.al341823.telefonia.facturacion.tarifas.TarifaDiasSemana;
+import es.uji.al341823.telefonia.facturacion.tarifas.TarifaHoras;
+
+/**
+ * Created by al341819 on 4/04/17.
+ */
+public class FabricaTarifas {
+    public Tarifa getTarifa(TipoTarifa tipo){
+        Tarifa tarifa=new TarifaBasica(0.15f);
+        switch(tipo){
+            case TARDES:
+                tarifa=new TarifaHoras(new TarifaBasica(0.15f),0.05f,16,20);
+                break;
+            case DOMINGOS:
+                tarifa=new TarifaDiasSemana(new TarifaBasica(0.15f),0.0f,7);
+                break;
+        }
+        return tarifa;
+    }
+}
